141b609d79c88ee4e8856fd6015e5c0a51db9025
PEDRO HENRIQUE DE BORTOLI <03298022080@senacrs.edu.br>
2024-03-05 12:01:50 -0300

    exercicios

---
+++ index.js
@@ -0,0 +1,50 @@
+const random1 = Math.floor(Math.random() * 100);
+const random2 = Math.floor(Math.random() * 100);
+
+console.log("NUMEROS SELECIONADOS")
+console.log(random1)
+console.log(random2)
+
+// --------------------------------------------------------- //
+
+const num1 = random1;
+const num2 = random2;
+
+console.log("SOMA")
+
+let soma = num1 + num2;
+
+console.log(soma);
+
+// --------------------------------------------------------- //
+
+const num3 = random1;
+const num4 = random2;
+
+console.log("SUBITRAÇÃO")
+
+let subtracao = num3 - num4;
+
+console.log(subtracao);
+
+// --------------------------------------------------------- //
+
+const num5 = random1;
+const num6 = random2;
+
+console.log("DIVISÃO")
+
+let divisao = num5/num6;
+
+console.log(divisao);
+
+// --------------------------------------------------------- //
+
+const num7 = random1;
+const num8 = random2;
+
+console.log("MULTIPLICAÇÃO")
+
+let multiplicacao = num5 * num6;
+
+console.log(multiplicacao);
